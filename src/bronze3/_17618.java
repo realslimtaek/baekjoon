888d6ce2a9a31fca0c7727625115ce148b4e3fc6
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-03-11 09:47:26 +0900

    Problem Solved

---
+++ src/bronze3/_17618.java
@@ -0,0 +1,34 @@
+package bronze3;
+import java.util.*;
+import java.io.*;
+public class _17618 {
+    static int n;
+    static int cnt = 0;
+
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        n = Integer.parseInt(br.readLine());
+
+        for(int i = 1; i<=n; i++)
+            solution(i);
+
+        System.out.println(cnt);
+    }
+
+    public static void solution(int a){
+
+        int a_divided = 0;
+        int temp = a;
+
+
+        while(temp != 0){
+            a_divided += temp % 10;
+            temp /= 10;
+        }
+
+        if(a % a_divided == 0) cnt++;
+
+
+    }
+}
