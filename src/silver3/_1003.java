8ebdffd6909a8411ef2ed72e23c3bc7df0d22239
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-04-17 15:22:41 +0900

    feat

--- src/silver3/_1003.java
+++ src/silver3/_1003.java
@@ -10,13 +10,13 @@ public class _1003 {
         int n = sc.nextInt();
         arr[1] = 1;
         arr[2] = 1;
-        for(int i = 3; i<41; i++) {
+        for (int i = 3; i < 41; i++) {
             arr[i] = arr[i - 1] + arr[i - 2];
         }
 
-        for(int i = 0; i<n; i++){
+        for (int i = 0; i < n; i++) {
             int a = sc.nextInt();
-            System.out.println(a-1 >= 0 ? arr[a-1]+" "+ arr[a] : "1 " + arr[a]);
+            System.out.println(a - 1 >= 0 ? arr[a - 1] + " " + arr[a] : "1 " + arr[a]);
         }
 
 
