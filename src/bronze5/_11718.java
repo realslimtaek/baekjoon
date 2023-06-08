cead4c202a436aa034bef428455f7e334cc532b2
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-06-08 15:00:30 +0900

    solved

---
+++ src/bronze5/_11718.java
@@ -0,0 +1,16 @@
+package bronze5;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class _11718 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        while(sc.hasNext()){
+            System.out.println(sc.nextLine());
+        }
+    }
+}
