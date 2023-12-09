b8a71c98d30c60137fca43a0349f1682941d65a7
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-12-09 12:29:22 +0900

    fixed

--- src/bronze3/_17618.java
+++ src/bronze3/_17618.java
@@ -3,33 +3,21 @@ import java.util.*;
 import java.io.*;
 public class _17618 {
     //정올 2019 2차 1번 https://www.acmicpc.net/problem/17618
-    static int n;
-    static int cnt = 0;
-
     public static void main(String[] args) throws Exception{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        n = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
+        int cnt = 0;
 
-        for(int i = 1; i<=n; i++)
-            solution(i);
-
-        System.out.println(cnt);
-    }
-
-    public static void solution(int a){
-
-        int a_divided = 0;
-        int temp = a;
-
-
-        while(temp != 0){
-            a_divided += temp % 10;
-            temp /= 10;
+        for(int i = 1; i<=n; i++){
+            int sum = 0;
+            for(char asdf : Integer.toString(i).toCharArray()){
+                sum += asdf-48;
+            }
+            if(i % sum == 0) cnt++;
         }
 
-        if(a % a_divided == 0) cnt++;
 
-
+        System.out.println(cnt);
     }
 }
